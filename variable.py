14dcdbbad5a4b1c49328437d37427acbe2e1cb92
janetbabu28-lang <janetbabu28@gmail.com>
2026-04-11 12:49:54 +0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,11 @@
+x = 5
+y = "gia"
+print ("The value of x is", x)
+print ("The value of y is", y)
+codingal = "codingal"
+print (codingal)
+name = input ("Please enter your name ")
+print ("Hello", name, "\n Have fun coding")
+
+import keyword
+print (keyword.kwlist)
---
+++ welcome.py
@@ -0,0 +1,11 @@
+#Enter sentence
+print ("Welcome to coding")
+# print number
+print ("7")
+#print sentence with a new line
+print ("Coding is fun \n")
+#print with two arguments
+print ("Let's start coding",5)
+#end argument with a symbol
+print ("Welcome",end="*" )
+print ("gia")
