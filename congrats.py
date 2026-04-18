8852ae9c2b4eecb862f2a5887ec174ebb30f9b5d
janetbabu28-lang <janetbabu28@gmail.com>
2026-04-18 10:50:29 +0800

    Add files via upload

---
+++ congrats.py
@@ -0,0 +1,12 @@
+ name = input("Enter your name: ")
+achievement = input("Enter your achievement")
+
+print("Name of student is",name)
+print ("Achievement of student is",achievement)
+
+print ("The name of students is", type(name))
+print ("The achievement of",name "is", type(age))
+
+
+name = str(name)
+print("Congratulations",type(age),"on",achievement)
