99603424189e2a9cac96dcc650b491ac2722d5a3
janetbabu28-lang <janetbabu28@gmail.com>
2026-04-27 16:43:01 +0800

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,7 @@
+temp = int(input("Please enter the current temperature of your surroundings: "))
+print ("The current temperature of your surroundings is: ",temp)
+
+if temp <25:
+    print("Please keep warm and eat hot food")
+else:
+    print("Stay cool and drink plenty of water")
