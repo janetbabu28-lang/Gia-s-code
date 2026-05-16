542af52093bc86367302c4cec144c3266d2c948e
janetbabu28-lang <janetbabu28@gmail.com>
2026-05-16 12:54:51 +0800

    Add files via upload

---
+++ infinite.py
@@ -0,0 +1,19 @@
+# nubers 1 to 100 in while loop
+i = 1
+while i <= 100:
+    print(i)
+    i += 1  
+
+# multipliction table using while loop
+num = int(input("Enter a number: "))
+i = 1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i = i + 1
+# even numbers using while loop
+n = 2
+while n <= 20:
+    print(n)
+    n = n+2 
+
+
---
+++ whileloop.py
@@ -0,0 +1,9 @@
+number = int(input("Please enter a number: "))
+
+sum = 0
+i = 1
+
+while i<=number:
+    sum = sum+i
+    i = i+1
+print("The total is: ", sum)    
